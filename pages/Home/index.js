f8bfbe8373c436f86d598a8c90135cdb2ce62778
Quasilius-Starlord <joysen833@gmail.com>
2022-06-22 04:28:58 -0700

    metamask

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -93,10 +93,10 @@ function Home(props){
 
     return(
     	<div style={{display:'flex',justifyContent:'space-evenly',flexDirection:'column',alignItems:'center',position:'relative',minHeight:'100vh'}}>
-            <div style={{width:'70%'}}>Lorem Ipsum is simply dummy text of the printing and typ
-            esetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-            , when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap i
-            nto electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset</div>
+            <div style={{width:'70%'}}>
+                <h1>Etherem Virtual KYC</h1>
+                <h5>Install <a href="https://metamask.io/download/" target={'_blank'}></a>Meta Mask wallet extention to use our services.</h5>
+            </div>
             <div style={{width:'50%'}}>
                 <Form onSubmit={e=>e.preventDefault()}>
                     <Form.Group style={{display:'flex', justifyContent:'space-between'}}>
